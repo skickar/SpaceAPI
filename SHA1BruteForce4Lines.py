c14f458e59544f5d54c92e2057a8e6c2ceee2532
Skickar <40251293+skickar@users.noreply.github.com>
2018-06-15 00:15:43 -0700

    added lasers to the roomba and camo
    stripped a line

--- SHA1BruteForce4Lines.py
+++
@@ -1,4 +0,0 @@
-from urllib.request import urlopen, hashlib
-origin = input("Please input the hash to crack.\n>")
-for password in str(urlopen('https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt').read(), 'utf-8').split('\n'):
-    [print("The password is ", str(password)), quit()] if (hashlib.sha1(bytes(password, 'utf-8')).hexdigest()) == origin else print("Password not in database, we'll get them next time.") if password == "" else print("Password guess ",str(password)," does not match, trying next...")
